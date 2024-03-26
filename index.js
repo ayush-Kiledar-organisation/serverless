475f692fdf2c847c0a5e60602a17fd003e4a0ae4
Ayush Kiledar <kiledar.ay@northeastern.edu>
2024-03-26 00:57:43 -0400

    commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const functions = require('@google-cloud/functions-framework');
 const Mailgun = require('mailgun.js');
 const FormData = require('form-data');
+const Verify = require('./model/verification');
+const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
 const API_KEY = process.env.API_KEY
@@ -20,13 +22,47 @@ functions.cloudEvent('helloPubSub', cloudEvent => {
     key: API_KEY,
     });
 
+  const tracking = async() =>{
+      const credentials = new Sequelize(
+
+        process.env.db_database,
+        process.env.db_username, 
+        process.env.db_password,{
+    
+            host: process.env.db_host,
+            dialect: 'mysql',
+            logging: false
+        });
+
+      const Schema = credentials.define('verify',Verify);
+
+      const obj = {
+        email: payload.email,
+        send_time: new Date(),
+      }
+
+      try{
+        credentials.sync({});
+        console.log("Table created");
+      }
+      catch(err){
+          console.log("Table creation error");
+      }
+
+    const verification = await Schema.create(obj);
+  }
+
   const mailFunc = async () => {
-    await mailgun.messages.create(DOMAIN, {
-        to: payload.email,
-        from: 'ayushkiledar10@gmail.com',
-        subject: 'User Account Verification- Webapp',
-        text: "Welcome. To access your account, verify your email with this link: http://ayush-kiledar-webapp:3000/verify?token="+payload.id+"&time="+payload.time
-    });
+
+      const verification = await Schema.create(obj);
+
+      await mailgun.messages.create(DOMAIN, {
+          to: payload.email,
+          from: 'ayushkiledar10@gmail.com',
+          subject: 'User Account Verification- Webapp',
+          html: `<h1>Welcome</h1><p>To access your account, verify your email with this link: <a href="http://ayush-kiledar-webapp:3000/verify?token=${payload.id}">Click here</a></p>`
+      });
     }
     mailFunc();
+    tracking();
 });
